71fb7ce732f88234343149de21bc65471183f636
Jude E Fairchild <jfairch81@ccs.local>
2019-10-09 11:05:40 -0400

    LCD Button Code Commented

--- LCD_Button.py
+++ LCD_Button.py
@@ -15,15 +15,15 @@ from lcd.lcd import CursorMode #pylint: disable-msg=import-error
 
 lcd = LCD(I2CPCF8574Interface(0x27), num_rows=2, num_cols=16)
 
-inputPin = digitalio.DigitalInOut(board.D9)
+inputPin = digitalio.DigitalInOut(board.D9) # This is how I got the button to work in Adafruit CircuitPython
 inputPin.direction = digitalio.Direction.INPUT
 inputPin.pull = digitalio.Pull.UP
 
-button = digitalio.DigitalInOut(board.D8)
+button = digitalio.DigitalInOut(board.D8) # This is how got the switch to work in Adafruit CircuitPython
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-counter = 0
+counter = 0 # Simple varriables I used
 oldSwitchState = 0
 newSwitchState = 1
 adder = 1
@@ -31,24 +31,24 @@ while True:
 
     lcd.clear()
     lcd.set_cursor_pos(0,0)
-    lcd.print("Popeyes Line: ")
+    lcd.print("Popeyes Line: ") #Prints this then how many people are in the "Popeyes Line: "
 
     lcd.set_cursor_pos(1,0)
-    lcd.print(str(counter))
+    lcd.print(str(counter)) #Prints the counter on the LCD Screen
 
     if button.value:
         print("0")
         time.sleep(0.05)
-        oldSwitchState = 0
+        oldSwitchState = 0 #sets it to run the Elif by making oldSwitchState 0
 
 
     elif oldSwitchState == 0 and newSwitchState == 1:
         if inputPin.value:
-            adder = 1
+            adder = 1 #adds to the adder which is then sets the counter to adder +1
         else:
             adder = -1
         print("1")
-        print(str(adder))
+        print(str(adder)) # Prints the adder to the serial monitor
         time.sleep(0.05)
         counter += adder
-        oldSwitchState = 1
+        oldSwitchState = 1 #sets the oldSwitchState back to 1 which should not run the Elif
