55e39350186299b9a688cfd95a500548b8c48135
Jude E Fairchild <jfairch81@ccs.local>
2019-10-09 11:05:53 -0400

    LED Fad code Commented

--- LED_Fade.py
+++ LED_Fade.py
@@ -1,11 +1,12 @@
 # Jude Fairchild
+# Have an LED Fade in and out
 # 8/22/19
 # Adapted from Phillip's code
 
 
 # Write your code here :-)
-import board
-import analogio
+import board #pylint: disable-msg=import-error
+import analogio #pylint: disable-msg=import-error
 
 #dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
@@ -15,7 +16,7 @@ led = analogio.AnalogOut(board.A0)
 while True:
 
     for i in range (31000,42000,1): # I use 31000 - 42000 because it shows the highest difference in LED lighting
-    led.value = i
+        led.value = i
 
     for i in range (42000,31000,-1): # I found the way to make it fade out was to switch the direction of the 42 and 31 and then make it -1 not 1
         led.value = i
