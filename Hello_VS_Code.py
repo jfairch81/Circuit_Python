b7eb293b2c63bfab76e28755ed498fc8c227eb1b
Jude E Fairchild <jfairch81@ccs.local>
2019-10-09 11:05:25 -0400

    Hello VS Code Commented

--- Hello_VS_Code.py
+++ Hello_VS_Code.py
@@ -1,6 +1,10 @@
+# Jude Fairchild
+# First step of VS Code, print out "Hello" in the Serial Monitor
+# 10/27/19
+
 import time
 
 
-while True:
+while True: # This is my first time on VS Code and it just prints "Hello"
     print("Hello")
     time.sleep(1)
