b898c2218d51d573e2cecfd22301cf26a6204cf9
Jude E Fairchild <jfairch81@ccs.local>
2019-10-09 11:05:11 -0400

    Fancy LED Commented

--- fancyLED.py
+++ fancyLED.py
@@ -1,13 +1,17 @@
+# Jude Fairchild
+# Have multiple LED's perform different actions
+# 10/2/19
+
 import time #pylint: disable-msg=import-error
 import board #pylint: disable-msg=import-error
 from digitalio  import Direction, Pull, DigitalInOut #pylint: disable-msg=import-error
 import random
 
-class FancyLED:
+class FancyLED: # I used a class because I called it in "main FancyLED" and that is how it knows what all the variablees are and how to run them.
 
-    def __init__(self,p1,p2,p3):
+    def __init__(self,p1,p2,p3): #this defines and Initializes p1,p2,p3
 
-        self.p1 = DigitalInOut(p1)
+        self.p1 = DigitalInOut(p1) #How I get p1 to be an LED
         self.p1.direction = Direction.OUTPUT
 
         self.p2 = DigitalInOut(p2)
@@ -16,7 +20,7 @@ class FancyLED:
         self.p3 = DigitalInOut(p3)
         self.p3.direction = Direction.OUTPUT  
     
-    def alternate(self):
+    def alternate(self): # Alternate - It changes between the lights in a 1-2-1 pattern
         self.p1.value = True
         self.p2.value = False
         self.p3.value = True
@@ -28,7 +32,7 @@ class FancyLED:
         time.sleep(.25)
         self.p2.value = False
 
-    def blink(self):
+    def blink(self): # Blink - It changes from On to Off
         self.p1.value = True
         self.p2.value = True
         self.p3.value = True
@@ -37,12 +41,12 @@ class FancyLED:
         self.p2.value = False
         self.p3.value = False
 
-    def on(self):
+    def on(self): # This was my test variable which turned everything on
         self.p1.value = True
         self.p2.value = True
         self.p3.value = True
     
-    def chase(self):
+    def chase(self): # This starts and has the led essentially move across the screen and turn off
         self.p1.value = True
         time.sleep(.25)
         self.p1.value = False
@@ -53,13 +57,13 @@ class FancyLED:
         time.sleep(.25)
         self.p3.value = False
     
-    def sparkle(self):
+    def sparkle(self): # I used a randomizer to random from 1 - 3 and have that determine whether the light turns on or not
         n = random.randint(1,3)
 
-        if n == 1:
+        if n == 1: # If the random number is 1 then only the first light turns on
             self.p1.value = True
             self.p2.value = False
-            self.p3.value = True
+            self.p3.value = False
             time.sleep(.15)
         
         if n == 2:
